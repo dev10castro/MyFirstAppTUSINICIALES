4aa784ed24230a627cacfbcb0664db28c459ddd9
dev10castro <dcassor814@iescarrillo.es>
2024-09-26 14:58:30 +0200

    prueba

--- app/src/main/java/com/example/myfirstapptusiniciales/SecondActivity.java
+++ app/src/main/java/com/example/myfirstapptusiniciales/SecondActivity.java
@@ -1,9 +1,12 @@
 package com.example.myfirstapptusiniciales;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,7 +34,55 @@ public class SecondActivity extends AppCompatActivity {
         Button send = findViewById(R.id.buttonSend);
         Button back = findViewById(R.id.buttonBack);
 
+    back.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intenttoMainctivity = new Intent(SecondActivity.this,MainActivity.class);
+            startActivity(intenttoMainctivity);
+        }
 
+
+    });
+
+    send.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if(name.getText().toString().isEmpty()){
+                Toast toastName = new Toast(SecondActivity.this);
+                toastName.setText("Name can´t be empty");
+                toastName.show();
+            } else if (yourAge.getText().toString().isEmpty()) {
+                Toast toastAge = new Toast(SecondActivity.this);
+                toastAge.setText("Age can´t be empty");
+                toastAge.show();
+
+            }else if(stature.getText().toString().isEmpty()){
+                Toast toastStature = new Toast(SecondActivity.this);
+                toastStature.setText("Stature can´t be empty");
+                toastStature.show();
+            }else {
+
+                Intent intenttoThirdActivity = new Intent(SecondActivity.this, ThirdActivity.class);
+
+                intenttoThirdActivity.putExtra("name",name.getText().toString());
+                intenttoThirdActivity.putExtra("age",yourAge.getText().toString());
+                intenttoThirdActivity.putExtra("stature",stature.getText().toString());
+
+                if(sw1.isChecked()){
+                    intenttoThirdActivity.putExtra("upto18","Soy mayor de 18");
+                }else{
+                    intenttoThirdActivity.putExtra("upto18","No soy mayor de 18");
+                }
+
+                startActivity(intenttoThirdActivity);
+            }
+
+
+        }
+    });
+
+    ;
 
 
 
--- app/src/main/java/com/example/myfirstapptusiniciales/ThirdActivity.java
+++ app/src/main/java/com/example/myfirstapptusiniciales/ThirdActivity.java
@@ -1,6 +1,11 @@
 package com.example.myfirstapptusiniciales;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,36 @@ public class ThirdActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button backMain = findViewById(R.id.buttonmain);
+        TextView nameSur = findViewById(R.id.textPutIm);
+        TextView age = findViewById(R.id.textPutAge);
+        TextView staTure = findViewById(R.id.textPutStature);
+        TextView agemore18 = findViewById(R.id.textMayorEdad);
+
+
+        Intent intenttoThirdActivity = getIntent();
+
+        String sname = intenttoThirdActivity.getStringExtra("name");
+        nameSur.setText("Soy "+sname+".");
+        String sage = intenttoThirdActivity.getStringExtra("age");
+        age.setText("Tengo "+sage+" años.");
+        String statu = intenttoThirdActivity.getStringExtra("stature");
+        staTure.setText("Mido "+statu+" metros.");
+        String sage18 = intenttoThirdActivity.getStringExtra("upto18");
+        agemore18.setText(sage18);
+
+
+
+        backMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intenttoMain = new Intent(ThirdActivity.this, MainActivity.class);
+
+                startActivity(intenttoMain);
+            }
+        });
+
     }
 }
